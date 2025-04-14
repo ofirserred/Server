6f58772035e5acbfb512ec51120853fb99e2c206
Ofir <ofirserred@gmail.com>
2025-04-14 19:47:44 +0300

    Add User model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,9 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  name: String,
+  email: String,
+  password: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
