8bda46d8e7ca66b855b491dff6428ba28b9420f1
Ofir <ofirserred@gmail.com>
2025-04-16 11:52:26 +0300

    added login logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,19 @@
-// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const { createUser, getUsers } = require('../controllers/userController');
 
+const {
+  createUser,
+  getUsers,
+  loginUser
+} = require('../controllers/userController');
+
+// יצירת משתמש חדש
 router.post('/users', createUser);
+
+// התחברות
+router.post('/login', loginUser);
+
+// שליפת כל המשתמשים (לא חובה לפרודקשן, רק לצורכי בדיקה)
 router.get('/users', getUsers);
 
 module.exports = router;
