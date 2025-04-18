9928fbdcadda55a80d595525f2bfbdfd9dc9748c
Ofir <ofirserred@gmail.com>
2025-04-19 00:17:37 +0300

    added loging logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,18 @@
-// controllers/userController.js
 const User = require('../models/User');
+const StudentProfile = require('../models/StudentProfile');
+const TeacherProfile = require('../models/TeacherProfile');
 
 // יצירת משתמש חדש
 const createUser = async (req, res) => {
   try {
-	const { email, password, role } = req.body;
-	const newUser = new User({ email, password, role });
+    const { email, password, role } = req.body;
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: "משתמש עם מייל זה כבר קיים" });
+    }
+
+    const newUser = new User({ email, password, role });
     await newUser.save();
 
     res.status(201).json({
@@ -22,6 +28,7 @@ const createUser = async (req, res) => {
   }
 };
 
+// התחברות
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -35,23 +42,30 @@ const loginUser = async (req, res) => {
       return res.status(401).json({ success: false, message: "סיסמה שגויה" });
     }
 
-	return res.status(200).json({
-  success: true,
-  message: "התחברת בהצלחה",
-  user: {
-    email: user.email || "",
-    role: user.role || ""
-  }
-});
+    let hasProfile = false;
 
-  } 
-  catch (error) 
-  {
+    if (user.role === 'teacher') {
+      const profile = await TeacherProfile.findOne({ userEmail: email });
+      hasProfile = !!profile;
+    } else if (user.role === 'student') {
+      const profile = await StudentProfile.findOne({ userEmail: email });
+      hasProfile = !!profile;
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "התחברת בהצלחה",
+      user: {
+        email: user.email,
+        role: user.role,
+        hasProfile
+      }
+    });
+
+  } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
-
-
 
 // שליפת כל המשתמשים
 const getUsers = async (req, res) => {
