15c35361e20f01535baf382ef2adc21534e54bdd
Ofir <ofirserred@gmail.com>
2025-05-06 01:21:10 +0300

    updating delete student profile logic

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createStudentProfile, getStudentProfileByEmail } = require("../controllers/studentProfileController");
+const { createStudentProfile, getStudentProfileByEmail, deleteStudentProfile } = require("../controllers/studentProfileController");
 
 router.post("/student-profile", createStudentProfile);
 router.get("/student-profile", getStudentProfileByEmail); // ⬅️ נוספה שורה זו
+router.post("/delete-profile",   deleteStudentProfile);  // ← הוסיפו את ה-route הזה
+
 
 module.exports = router;
